0bcde9b60223c201f8f6d5cd585c5d592bc89c0a
hec <hecatsia@gmail.com>
2020-09-28 19:23:27 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <baidu-map id="allmap"
+               :style="getMapStyle"
+               :center="center"
+               :zoom="zoom"
+               @ready="handler"
+    >
+      <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+    </baidu-map>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {getNodeData} from "@/api/static";
+import {waydata} from "../public/staticdata/waydata";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  computed: {
+    getMapStyle() {
+      // let width = document.body.clientWidth;
+      let height = document.body.clientHeight;
+      let option = {
+        'width': '100%',
+        'height' : height.toString() + 'px',
+      };
+      return option;
+    }
+  },
+  data () {
+    return {
+      center: {lng: 116.404, lat: 39.915},
+      zoom: 15,
+      BMap: null,
+      map: null,
+      nodes: null
+    }
+  },
+  created() {
+    getNodeData().then(res=>{
+      if (res) {
+        this.nodes = res;
+      }
+    }).catch(err => {
+      this.$message.error("获取地图信息错误：" + err);
+    })
+  },
+  methods: {
+    handler ({BMap, map}) {
+      this.BMap = BMap;
+      this.map = map;
+      let ways = waydata;
+
+      setTimeout(()=>{
+        let cpoint = new BMap.Point(this.nodes[ways['1'][1]].x, this.nodes[ways['1'][1]].y);
+        map.setCenter(cpoint);
+        for (let i = 1; i < 3479; i++) {
+          let pointstodraw = [];
+          for (let j = 0; j < ways[i].length; j++){
+            pointstodraw.push(new BMap.Point(this.nodes[ways[i][j]].x, this.nodes[ways[i][j]].y))
+          }
+          let polyline = new BMap.Polyline(pointstodraw, {strokeColor:"green", strokeWeight:5, strokeOpacity:0.8});   //创建折线
+          map.addOverlay(polyline);          //增加折线
+        }
+      }, 1000)
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#app,html,body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  padding: 0;
+  margin: 0;
 }
+
+#allmap {
+  width: 100%;
+  height: 100%;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BaiduMap from 'vue-baidu-map'
+import Axios from "axios";
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入element-ui样式
+
+Vue.use(Element);
+Vue.use(BaiduMap, {
+  // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+  ak: '7UNbz4033giyAA5fNTd36Hihu02WdHXP'
+})
 
 Vue.config.productionTip = false
+Vue.prototype.$http = Axios;
 
 new Vue({
   render: h => h(App),
